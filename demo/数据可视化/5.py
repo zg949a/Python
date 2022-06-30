b6c62d3c5f1440499092a02a8ee827f8bf439407
zg949a <99232614+zg949a@users.noreply.github.com>
2022-06-30 08:17:29 -0400

    Delete demo directory

--- demo/数据可视化/5.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-
-x_values = range(1, 1001)
-y_values = [x ** 2 for x in x_values]
-plt.style.use('seaborn')
-fig, ax = plt.subplots()
-ax.scatter(x_values, y_values, c=(0, 0.8, 0), s=10)
-ax.axis([0, 1000, 0, 1100000])
-plt.show()
--- demo/数据可视化/9.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-filename = "./ClassInfo.csv"
-with open(filename) as f:
-    reader = csv.reader(f)
-    header_row = next(reader)
-    for index, column_header in enumerate(header_row):
-        print(index+1, column_header)
